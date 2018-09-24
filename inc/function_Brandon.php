7d83639d72c067417d67f8c8903f1ecea1e0e6e8
Brandon Mendel <bmendel@csumb.edu>
2018-09-24 18:19:35 +0000

    Finished creating Deck class

--- inc/function_Brandon.php
+++ inc/function_Brandon.php
@@ -6,46 +6,52 @@
     }
     
     class Deck {
-        public $cards;
-        public $size;
         
-        function Deck() {
-            $cards = array();
-            $size = 52;
+        public $deck;
+        
+        function __construct() {
             $this->createDeck();
         }
         
         function createDeck() {
+            $this->deck = array();
             $suit = array("clubs", "diamonds", "hearts", "spades");
         
-            for ($i = 0; $i < $size; $i++) {
+            for ($i = 0; $i < 52; $i++) {
                 $card = new Card();
-                $card->name = $suit[$i + 1] + '/' + ($i+1)%13;
-                $card->point = ($i+1)%13;
-                $cards[] = $card;
+                $card->name = $suit[$i/13].'/'.($i%13+1);
+                $card->point = ($i%13+1);
+                $this->deck[$i] = $card;
             }
         }
         
         function printDeck() {
-            echo "Why does this work for '$size' <br />";
-            for ($i = 0; $i < $size; $i++) {
-                echo "And why doesn't this work <br />";
-                $card = $cards[$i];
-                echo $card->name . "  " . $card->value . "<br />";
+            foreach ($this->deck as $card) {
+                echo "$card->name  $card->point<br />";
             }
         }
         
         function shuffleDeck() {
-            shuffle($cards);
+            shuffle($this->deck);
         }
         
         function drawFromDeck() {
-            return array_pop($cards);
+            return array_pop($this->deck);
         }
     }
     
     function testDeck() {
-        $deck = new Deck;
+        $deck = new Deck();
+        $deck->shuffleDeck();
+        for ($i = 0; $i < 48; ++$i) {
+            $card = $deck->drawFromDeck();
+            echo "Drew a $card->name<br />";
+        }
+        echo "Remaining deck:<br />";
+        $deck->printDeck();
+        
+        $deck->createDeck();
+        echo "Newly created deck:<br />";
         $deck->printDeck();
     }
     
