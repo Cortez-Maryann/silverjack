bfc69d10b9c59d76d64740d39eba039cb40a3a7c
Brett Stevenson <bstevenson@csumb.edu>
2018-09-26 00:14:24 +0000

    Add tags for page formatting

--- index.php
+++ index.php
@@ -7,9 +7,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat"/>
+      <link rel="stylesheet" type="text/css" href="./css/styles.css"/>
+      <title>SilverJack</title>
     </head>
     <body>
+      <header><h1>SilverJack</h1></header>
         <?php
           $deck = new Deck();
           $players = array("Maryann", "Antonio", "Brandon", "Brett");
@@ -21,7 +24,9 @@
 
             for($i=0; $i<count($players); $i++) {
                 echo "<div class='row'>";
-                echo "<h4 class='player'>$players[$i]</h4>";
+                echo "<div class='player-container'>";
+                echo "<img src='./img/avatar.jpg' class='player-img' alt='$player[$i]'/>";
+                echo "<h4 class='player-title'>$players[$i]</h4></div>";
                 $hand = getHand();
                 $total = array_pop($hand);
                 $totals[$players[$i]] = $total;
