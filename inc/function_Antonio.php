3df73811380b5d9f1493458b6291fe306052bb56
Antonio Vega <antvega@csumb.edu>
2018-10-03 20:00:10 +0000

    Updated my displayWinner() function

--- inc/function_Antonio.php
+++ inc/function_Antonio.php
@@ -1,43 +1,77 @@
 <?php
-    class Card {
-        public $suit;
-        public $value;
-    }
-    $values = range(0,51);
-    $suits = array("hearts","spades", "clubs","diamonds");
-    $deck = [];
+    global $totals;
+    global $players;
+    // class Card {
+    //     public $suit;
+    //     public $value;
+    // }
+    // $values = range(0,51);
+    // $suits = array("hearts","spades", "clubs","diamonds");
+    // $deck = [];
     
-    function makeDeck(){
-        for($i = 0; $i < 4;$i++){
-            for($j = 0; $j<13; $j++) {
-                $n = new Card;
-                $n->$suit = $suits[$i];
-                $n->$value = $i;
-                array_push($deck,$n);
-                //$deck[] = $n;
+    // function makeDeck(){
+    //     for($i = 0; $i < 4;$i++){
+    //         for($j = 0; $j<13; $j++) {
+    //             $n = new Card;
+    //             $n->$suit = $suits[$i];
+    //             $n->$value = $i;
+    //             array_push($deck,$n);
+    //             //$deck[] = $n;
+    //         }
+    //     }
+        
+    // }
+    
+    
+    
+    function displayWinner(){
+        global $totals;
+        global $players;
+        $highestScore=0;
+        //this will get the highest score
+        foreach ($totals as $value) {
+            if($highestScore<$value){
+                $highestScore=$value;
+            }
+        }
+
+    
+        //next we have to check if there are multiple winners. If there are, all winners will 
+        // be added to $tiedWinners
+        $winnersNames = array();
+        $tiedWinners = array();// dictionary of winners and their scores
+        
+        /////// NOT SURE IF THIS SYNTAX IS 100%  CORRECT
+        foreach ($totals as $key=>$value) {
+            if($highestScore==$value){
+                $tiedWinners[$key]=$value;
             }
         }
         
-    }
+        //////******** IGNORE **********/////////
+        // for($i=0;$i<count($totals);$i++){
+        //     if($highestScore==$totals[$players[$i]]){
+        //         $tiedWinners[$players[$i]]=$totals[$players[$i]];
+        //         $winnersNames[]=$players[$i];
+        //     }
+        // }
+        //*************************************//
+        
+        // added score of all people who didnt win
+        $winnersFinalScore;
+        foreach ($totals as $key => $value) {
+            foreach ($tiedWinners as $key2 => $value2) {
+                if ($key!=$key2){
+                    $winnersFinalScore+=$value;
+                }
+            }
+        }
     
-    function display(){
-        
-        // for($i = 0; $i < 53;$i++){
-        //     print_r($deck);
-        // }
-        //echo "<h1>hello<h2>";
-        // for($i = 0; $i < count($deck); $i++){
-        //     echo $deck[$i]->$suit;
-        //     echo $deck[$i]->$value;
-        // }
-        
-        echo count($deck);
-       // echo print_r($deck->$suit);
+        // display winners(if there are more than one)
+        foreach ($tiedWinners as $key=>$value) {
+            echo $key. " has the score of ". $winnersFinalScore;
+        }
     }
-
-    // function findWinner(){
-    //     $scores["a"]
-    // }
 
 ?>
 
