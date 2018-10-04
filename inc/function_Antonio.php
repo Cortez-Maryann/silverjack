b601542ab7c4463d590c91c1f12879b25180c708
Antonio Vega <antvega@csumb.edu>
2018-10-04 04:00:24 +0000

    Cleaned code

--- inc/function_Antonio.php
+++ inc/function_Antonio.php
@@ -15,55 +15,20 @@
             }
             $everyPlayerScoreAdded+=$playerScore;
         }
-        // echo "highest score :". $highestScore;
-        // echo "<br>";
-        // echo "<br>";
-        // echo "<br>";
-        
-        //next we have to check if there are multiple winners. If there are, all winners will 
-        // be added to $tiedWinners
-        // $winnersNames = array();
+
         $tiedWinners = array();// dictionary of winners and their scores
-        
-        /////// NOT SURE IF THIS SYNTAX IS 100%  CORRECT
         foreach ($totals as $key=>$value) {
             if($highestScore==$value){
                 $tiedWinners[$key]=$value;
             }
         }
-        
-        //////******** IGNORE **********/////////
-        // for($i=0;$i<count($totals);$i++){
-        //     if($highestScore==$totals[$players[$i]]){
-        //         $tiedWinners[$players[$i]]=$totals[$players[$i]];
-        //         $winnersNames[]=$players[$i];
-        //     }
-        // }
-        //*************************************//
-        
-        // added score of all people who didnt win
-        
-        // $countr = 0;
-        // foreach ($totals as $key => $value) {
-        //     foreach ($tiedWinners as $key2 => $value2) {
-        //         if ($key!=$key2){
-        //             $countr++;
-        //             $winnersFinalScore+=$value;
-        //         }
-        //     }
-        // }
-        
+        // check for more than one winner
         if(count($tiedWinners)>1){
             $moreThanOneWinner=true;
         }
-        // echo "amount of winners: ".count($tiedWinners);
-        // echo "<br>";
 
-        // display winners(if there are more than one)
-        // echo "all scores added: ".$everyPlayerScoreAdded;
-        // echo "<br>";
+        // displaying winners
         foreach ($tiedWinners as $key=>$value) {
-            // echo "hello";
             echo $key. " has the score of ". ($everyPlayerScoreAdded-(count($tiedWinners)*$highestScore));
             echo "<br>";
         }
