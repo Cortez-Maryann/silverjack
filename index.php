b380de4b5363766ee3a4e2b9f672b91bb289d556
John Economides <jeconomides@csumb.edu>
2018-10-06 06:49:56 +0000

    John's version of SilverJack

--- index.php
+++ index.php
@@ -1,31 +1,9 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-
-<?php
-    include 'inc/function_John.php';
-?>
-
-<html>
-    <body>
-        <style>
-            @import url("css/styles.css");
-        </style>
-        
-        <div id="pageTitle">
-              <h1>FF1 SILVER JACK</h1>  
-        </div>
-        
-        <br><br>
-        <?php
-            displayPlayers();
-         ?>  
-        </div>
-=======
 <?php
   include './inc/function_Antonio.php';
   include './inc/function_Maryann.php';
   include './inc/function_Brandon.php';
   include './inc/function_Brett.php';
+  include './inc/function_John.php';
 ?>
 
 <!DOCTYPE html>
@@ -38,34 +16,12 @@
     <body>
       <header><h1>SilverJack</h1></header>
         <?php
+          //global $playerNames;
           $moreThanOneWinner=false;
           $deck = new Deck();
-          $players = array("Maryann", "Antonio", "Brandon", "Brett");
-          shuffle($players);
           $totals = array();
-          function play() {
-            global $deck;
-            global $players;
-            global $totals;
-
-            for($i=0; $i<count($players); $i++) {
-                echo "<div class='row'>";
-                echo "<div class='player-container'>";
-                echo "<img src='./img/avatar.jpg' class='player-img' alt='$player[$i]'/>";
-                echo "<h4 class='player-title'>$players[$i]</h4></div>";
-                $hand = getHand();
-                $total = array_pop($hand);
-                $totals[$players[$i]] = $total;
-                displayHand($hand, $total);
-                echo "</div>";
-            }
-            echo "<div class='winner-container'><h2>";
-            displayWinner();
-            echo "</h2></div>";
-            displayAverageTime();
-          }
+          
           play();
         ?>
->>>>>>> 2c12f20766d58d215fc81b57d7a48b80f997d232
     </body>
 </html>
